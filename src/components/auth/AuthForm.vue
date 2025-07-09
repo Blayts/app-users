4ae3538a9f0200f1bf6300e085ddb00d9e1d5059
Blayts <Grehovnik-18@yandex.ru>
2025-07-09 16:20:08 +0400

    Designed auth and register forms

--- src/components/auth/AuthForm.vue
+++ src/components/auth/AuthForm.vue
@@ -1,23 +1,69 @@
 <script setup lang="ts">
-import { Button } from 'ant-design-vue';
+import { Button, Form, FormItem, Input, InputPassword, Space } from 'ant-design-vue';
+import type { FormProps } from 'ant-design-vue';
+import { ref } from 'vue';
 import { useRouter } from 'vue-router';
-import { useAuth } from '../../hooks/useAuth';
+// import { useAuth } from '../../hooks/useAuth';
 
 const router = useRouter();
 
-const { login } = useAuth();
+// const { login } = useAuth();
+
+const model = ref({ password: '', username: '' });
+
+const rules: FormProps['rules'] = {    
+    password: {
+        message: 'Password can\'t be empty',
+        validator(_, value: string) {
+            return value ? Promise.resolve(): Promise.reject();
+        },
+    },
+    username: {
+        message: 'Username can\'t be empty',
+        validator(_, value: string) {
+            return value ? Promise.resolve(): Promise.reject();
+        }
+    },
+};
 
 function goToRegisterForm() {
     router.push('/register');
 }
+
+function handlerFinish(values: any) {
+    console.log('AUTHORIZE', values);
+    //login(model.login, model.password);
+}
 </script>
 
 <template>
-    <div>
-        <h3>Auth Form</h3>
-        <Button @click="login">Login</Button>
-        <Button @click="goToRegisterForm">Register</Button>
-    </div>    
+    <section>
+        <header>
+            <h3 style="text-align: center;">Authorize</h3>
+        </header>
+        <main>
+            <Form @finish="handlerFinish" :model="model" :rules="rules">
+                <FormItem label="Username" name="username" :label-col="{ span: 8 }">
+                    <Input 
+                        autocomplete="off" 
+                        v-model:value="model.username"
+                    ></Input>
+                </FormItem>
+                <FormItem label="Password" name="password" :label-col="{ span: 8 }">
+                    <InputPassword
+                        autocomplete="off" 
+                        v-model:value="model.password" 
+                    ></InputPassword>
+                </FormItem>
+                <FormItem :wrapper-col="{ offset: 8 }">
+                    <Space>
+                        <Button html-type="submit">Log in</Button>
+                        <Button @click="goToRegisterForm" type="link">Register</Button>
+                    </Space>
+                </FormItem>
+            </Form>
+        </main>
+    </section>
 </template>
 
 <style scoped>
--- src/components/auth/RegisterForm.vue
+++ src/components/auth/RegisterForm.vue
@@ -1,19 +1,92 @@
 <script setup lang="ts">
-import { Button } from 'ant-design-vue';
+import { Button, Form, FormItem, Input, InputPassword, Space } from 'ant-design-vue';
+import type { FormProps } from 'ant-design-vue';
 import { useRouter } from 'vue-router';
+import { ref } from 'vue';
 
 const router = useRouter();
+
+const model = ref({ name: '', password: '', repeatPassword: '', username: '' });
+
+const rules: FormProps['rules'] = {
+    name: {
+        message: 'Name must be filled',
+        required: true
+    },
+    password: {
+        message: 'Password must be filled',
+        required: true
+    },
+    repeatPassword: {
+        required: true,
+        validator: (_, value: string) => {
+            if(!value) {
+                return Promise.reject('Repeat must be filled');
+            }
+            else if(value !== model.value.password) {
+                return Promise.reject('The password doesn\'t match');
+            }
+            else {
+                return Promise.resolve();
+            }
+        }
+    },
+    username: {
+        message: 'Username must be filled',
+        required: true
+    }
+};
 
 function goBack() {
     router.back();
 }
+
+function handlerFinish(values: any) {
+    console.log('REGISTER', values);
+}
+
 </script>
 
 <template>
-    <div>
-        <h3>Register Form</h3>
-        <Button @click="goBack">Back</Button>
-    </div>
+    <section>
+        <header>
+            <h3 style="text-align: center;">Register new user</h3>
+        </header>
+        <main>
+            <Form @finish="handlerFinish" :model="model" :rules="rules">
+                <FormItem label="Username" name="username" :label-col="{ span: 8 }">
+                    <Input 
+                        autocomplete="off" 
+                        v-model:value="model.username"
+                    ></Input>
+                </FormItem>
+                <FormItem label="Name" name="name" :label-col="{ span: 8 }">
+                    <Input 
+                        autocomplete="off" 
+                        v-model:value="model.name"
+                    ></Input>
+                </FormItem>
+                <FormItem label="Password" name="password" :label-col="{ span: 8 }">
+                    <InputPassword
+                        autocomplete="off" 
+                        v-model:value="model.password" 
+                    ></InputPassword>
+                </FormItem>
+                <FormItem label="Repeat pass" name="repeatPassword" :label-col="{ span: 8 }">
+                    <InputPassword
+                        autocomplete="off" 
+                        v-model:value="model.repeatPassword" 
+                    ></InputPassword>
+                </FormItem>        
+                <FormItem :wrapper-col="{ offset: 8 }">
+                    <Space>
+                        <Button html-type="submit" type="primary">Register</Button>
+                        <Button @click="goBack">Back</Button>
+                    </Space>
+                </FormItem>
+            </Form>
+        </main>
+    </section>
 </template>
 
 <style scoped>
